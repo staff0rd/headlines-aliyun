2cb9e6419e7bc7a287eb5b688df7c04c79dbc828
Stafford Williams <stafford.williams@gmail.com>
2021-09-28 00:43:38 +1000

    completed persist

--- src/parseFeed.ts
+++ src/parseFeed.ts
@@ -6,8 +6,7 @@ export const handle = async (
   context,
   callback: (err: any, data: any) => void
 ) => {
-  console.log("event", event);
-  console.log("context", context);
+  //console.log("context", context);
   const feed = await parseFeed();
   await persist(feed, context);
   callback(null, `Retrieved ${feed.items.length} items from ${feed.title}`);
@@ -21,22 +20,23 @@ const parseFeed = async () => {
   return feed;
 };
 
-const persist = async (body: any, context: any) => {
+export const persist = async (body: any, context: any) => {
   const date = new Date();
   const key = `${date.getFullYear()}/${
     date.getMonth() + 1
   }/${date.getDay()}/${date.getHours()}${date.getMinutes()}.json`;
 
-  const client = new OSS({
-    region: context.region,
+  const config = {
+    region: `oss-${context.region}`,
     accessKeyId: context.credentials.accessKeyId,
     accessKeySecret: context.credentials.accessKeySecret,
     bucket: process.env.BUCKET_NAME,
     stsToken: context.credentials.securityToken,
-  });
+  };
+  const client = new OSS(config);
 
   try {
-    await client.put(key, OSS.Buffer(JSON.stringify(body, null, 2)));
+    await client.put(key, Buffer.from(JSON.stringify(body, null, 2)));
     console.log(`Saved ${key}`);
   } catch (err) {
     console.error("Failed to put", err);
