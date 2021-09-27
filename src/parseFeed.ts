4a8af84e1806d26731d5374614e143a164a7fe3a
Stafford Williams <stafford.williams@gmail.com>
2021-09-27 23:07:13 +1000

    implement persist

--- src/parseFeed.ts
+++ src/parseFeed.ts
@@ -1,4 +1,5 @@
 import Parser from "rss-parser";
+import OSS from "ali-oss";
 
 export const handle = async (
   event,
@@ -8,7 +9,7 @@ export const handle = async (
   console.log("event", event);
   console.log("context", context);
   const feed = await parseFeed();
-  //await persist(feed);
+  await persist(feed, context);
   callback(null, `Retrieved ${feed.items.length} items from ${feed.title}`);
 };
 
@@ -20,17 +21,24 @@ const parseFeed = async () => {
   return feed;
 };
 
-// const persist = async (body: any) => {
-//   const date = new Date();
-//   const key = `${date.getFullYear()}/${
-//     date.getMonth() + 1
-//   }/${date.getDay()}/${date.getHours()}${date.getMinutes()}.json`;
-//   await new S3Client({ region: process.env.AWS_REGION }).send(
-//     new PutObjectCommand({
-//       Bucket: process.env.BUCKET_NAME,
-//       Key: key,
-//       Body: JSON.stringify(body, null, 2),
-//     })
-//   );
-//   console.log(`Saved ${key}`);
-// };
+const persist = async (body: any, context: any) => {
+  const date = new Date();
+  const key = `${date.getFullYear()}/${
+    date.getMonth() + 1
+  }/${date.getDay()}/${date.getHours()}${date.getMinutes()}.json`;
+
+  const client = new OSS({
+    region: context.region,
+    accessKeyId: context.credentials.accessKeyId,
+    accessKeySecret: context.credentials.accessKeySecret,
+    bucket: process.env.BUCKET_NAME,
+    stsToken: context.credentials.securityToken,
+  });
+
+  try {
+    await client.put(key, OSS.Buffer(JSON.stringify(body, null, 2)));
+    console.log(`Saved ${key}`);
+  } catch (err) {
+    console.error("Failed to put", err);
+  }
+};
